f3557840dea0676902a3c06a54f16645936d34ca
Peilin Sun <46664832+PPPerry@users.noreply.github.com>
2022-10-19 16:36:09 +0800

    Update boolean_query.py

--- boolean_query.py
+++ boolean_query.py
@@ -1,43 +1,46 @@
 import json
+import pandas as pd
+
+# 加载数据
+data = open('data/index.json', 'r', encoding='utf-8') 
+index = json.load(data)  # 索引字典
+
+fileName = 'data/csl_40k.tsv'
+tsv_file = pd.read_csv(
+    fileName,
+    delimiter='\t',
+    names=["title", "abstract", "keywords", "discipline", "category"],
+    error_bad_lines=False,
+    encoding='utf-8'
+)
 
 def bool_retrieval(string):
-    data = open('data/index.json', 'r', encoding='utf-8') 
-    index = json.load(data)  # 索引字典
+    # and优先级大于or
+    if string.count('and')*string.count('or') > 0:
+        a = string[:string.find('or')]
+        b = string[string.find('or')+2:]
+        bool_retrieval(a)
+        bool_retrieval(b)
 
-    if 'or' in string:
-        key = string.split(' or ')
+    elif 'or' in string:
+        key = string.split('or')
         value = set(index[key[0]])
         for i in range(1, len(key)):
             value = value.union(set(index[key[i]]))
-        print(list(value))
+        for col in list(value):
+            print(col, tsv_file.iloc[col, 0])
+
     elif 'and' in string:
-        key = string.split(' and ')
+        key = string.split('and')
         value = set(index[key[0]])
         for i in range(1, len(key)):
             value = value.intersection(set(index[key[i]]))
-        print(list(value))
-    # if string.count('and')*string.count('or') > 0:
-    #     a = string[:string.find('or')]
-    #     b = string[string.find('or')+3:]
-    #     bool_retrieval(a)
-    #     bool_retrieval(b)
-    # elif 'or' in string:
-    #     key = string.split(' or ')
-    #     for i in range(len(documentbase)):
-    #         for j in range(len(key)):
-    #             if key[j] in documentbase[i]:
-    #                 print('D%d:'%(i+1),documentbase[i])
-    # elif 'and' in string:
-    #     key = string.split(' ')
-    #     del key[key.index('and')]
-    #     for i in range(len(documentbase)):
-    #         flag = 1
-    #         for j in range(len(key)):
-    #             if key[j] not in documentbase[i]:
-    #                 flag = 0
-    #                 break 
-    #         if(flag):
-    #             print('D%d:'%(i+1),documentbase[i])
-
+        for col in list(value):
+            print(col, tsv_file.iloc[col, 0])
+    
+    else:
+        value = set(index[string])
+        for col in list(value):
+            print(col, tsv_file.iloc[col, 0])
 
 bool_retrieval(input()) 
